e567b1d09e3f3bc9a076784a1f4898869689fd00
TutulDevs <tutulnahid@gmail.com>
2021-01-28 15:16:34 +0600

    makers: added exam-title input in examArea

--- makers/src/components/ExamArea.js
+++ makers/src/components/ExamArea.js
@@ -18,6 +18,8 @@ const ADD_QUES_COUNT = "ADD_QUES_COUNT";
 function ExamArea() {
   const [currentQuesID, setCurrentQuesID] = useState(2);
   const [mcqQuestionIDList, setmcqQuestionIDList] = useState([1]);
+  const [focusExamTitle, setFocusExamTitle] = useState(false);
+  const [examTitle, setExamTitle] = useState("Exam Title");
 
   function addAnotherQuestion() {
     setmcqQuestionIDList(mcqQuestionIDList.concat(currentQuesID));
@@ -35,8 +37,31 @@ function ExamArea() {
     };
   }
 
+  const examTitleHandler = (e) => {
+    setExamTitle(e.target.value);
+    console.log(e.target.value);
+  };
+
   return (
     <div className='ExamArea'>
+      <div
+        className={
+          focusExamTitle ? "exam-title-wrapper focused" : "exam-title-wrapper"
+        }
+      >
+        <label for='exam-title'>
+          <i className='fa fa-file'></i>
+          <span>Exam Title </span>
+        </label>
+        <input
+          placeholder='Type Exam Title'
+          id='exam-title'
+          onFocus={() => setFocusExamTitle(true)}
+          onBlur={() => setFocusExamTitle(false)}
+          onChange={examTitleHandler}
+          className='exam-title'
+        />
+      </div>
       {mcqQuestionIDList.map((idi) => (
         <McqQuestion id={idi} key={idi} />
       ))}
